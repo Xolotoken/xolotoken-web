9e0694bbc03bb25d5e065704246c86a7d1f8b73b
Jesus Rivera <chuy_1322@hotmail.com>
2021-07-06 01:03:38 -0600

    Banner ADDED, Countdown ADDED

--- script.js
+++ script.js
@@ -69,3 +69,28 @@ function copyAddress()
     document.execCommand('copy');
     document.body.removeChild(el);
 }
+
+
+var x = setInterval(function() {
+
+    var end = new Date('07/07/2021 1:00 PM');
+    var now = new Date();
+    var nowUTC = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+    var distance = end - nowUTC;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdownP").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdownP").innerHTML = "EXPIRED";
+  }
+}, 1000);
